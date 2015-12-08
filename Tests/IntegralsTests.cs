84aa1a746ce7428dff5499a233b413b4b42eaf82
Rasul <rasuldev@gmail.com>
2015-12-08 12:20:26 +0300

    integrals

---
+++ Tests/IntegralsTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using mathlib;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class IntegralsTests
+    {
+        private List<TestCase> TestCases;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            TestCases = new List<TestCase>()
+            {
+                new TestCase(x => x, 0, 1, 0.5, 1000, 0.01),
+                new TestCase(x => x, -1, 1, 0.0, 1000, 0.01),
+                new TestCase(x => x, -1, 1, 0.0, 10000, 0.001),
+                new TestCase(x => x, -1, 3, 4.0, 10000, 0.01),
+                new TestCase(x =>
+                {
+                    if (x < 0.125)
+                        return 1;
+                    if (x < 0.5)
+                        return -1;
+                    return 0;
+                }, 0, 1, 5, 0, 0.001)
+            };
+
+        }
+
+        public void RunTestCases(Func<TestCase, double> testedFunc)
+        {
+            foreach (var testCase in TestCases)
+            {
+                var value = testedFunc(testCase);
+                Assert.AreEqual(testCase.Result, value, testCase.Delta);
+            }
+        }
+
+        double[] GenerateNodes(double a, double b, int count)
+        {
+            var nodes = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                nodes[i] = a + i * (b - a) / (count - 1);
+            }
+            return nodes;
+        }
+
+        [Test]
+        public void RectangularLeft()
+        {
+            RunTestCases(testCase => Integrals.Rectangular(testCase.Func, testCase.A, testCase.B, testCase.NodesCount,
+                    Integrals.RectType.Left));
+        }
+
+        [Test]
+        public void RectangularRight()
+        {
+            RunTestCases(testCase => Integrals.Rectangular(testCase.Func, testCase.A, testCase.B, testCase.NodesCount,
+                    Integrals.RectType.Right));
+        }
+
+        [Test]
+        public void RectangularCenter()
+        {
+            RunTestCases(testCase => Integrals.Rectangular(testCase.Func, testCase.A, testCase.B, testCase.NodesCount,
+                        Integrals.RectType.Center));
+        }
+
+        [Test]
+        public void TrapezoidTest()
+        {
+            RunTestCases(
+                testCase => Integrals.Trapezoid(testCase.Func, GenerateNodes(testCase.A, testCase.B, testCase.NodesCount))
+            );
+        }
+
+        [Test]
+        public void TrapezoidEquiDistantNetTest()
+        {
+            RunTestCases(
+                testCase => Integrals.Trapezoid(testCase.Func, testCase.A, testCase.B, testCase.NodesCount)
+            );
+        }
+
+    }
+
+
+    public struct TestCase
+    {
+        public Func<double, double> Func { get; set; }
+        public double A { get; set; }
+        public double B { get; set; }
+        public double Result { get; set; }
+        public int NodesCount { get; set; }
+        public double Delta { get; set; }
+
+        public TestCase(Func<double, double> func, double a, double b, double result, int nodesCount, double delta)
+        {
+            Func = func;
+            A = a;
+            B = b;
+            Result = result;
+            NodesCount = nodesCount;
+            Delta = delta;
+        }
+    }
+}
---
+++ mathlib/Integrals.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace mathlib
+{
+    public static class Integrals
+    {
+        /// <summary>
+        /// Returns integral from nodes[0] to nodes[last] of function f.
+        /// </summary>
+        /// <param name="f"></param>
+        /// <param name="nodes"></param>
+        /// <param name="formulaType"></param>
+        /// <returns></returns>
+        public static double Rectangular(Func<double, double> f, double[] nodes, RectType formulaType)
+        {
+            double sum = 0;
+            double a, b;
+            switch (formulaType)
+            {
+                case RectType.Left:
+                    a = 1;
+                    b = 0;
+                    break;
+                case RectType.Right:
+                    a = 0;
+                    b = 1;
+                    break;
+                case RectType.Center:
+                    a = b = 0.5;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(formulaType), formulaType, null);
+            }
+
+            for (int i = 0; i < nodes.Length - 1; i++)
+            {
+                sum += (a * f(nodes[i]) + b * f(nodes[i + 1])) * (nodes[i + 1] - nodes[i]);
+            }
+            return sum;
+        }
+
+        public static double Trapezoid(Func<double, double> f, double[] nodes)
+        {
+            double sum = 0;
+            for (int i = 0; i < nodes.Length - 1; i++)
+            {
+                sum += (f(nodes[i + 1]) + f(nodes[i])) * (nodes[i + 1] - nodes[i]);
+            }
+            return sum / 2;
+        }
+
+        /// <summary>
+        /// Uses equidistant net
+        /// </summary>
+        /// <param name="f"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="nodesCount"></param>
+        /// <returns></returns>
+        public static double Trapezoid(Func<double, double> f, double a, double b, int nodesCount)
+        {
+            double sum = 0.5 * (f(a) + f(b));
+            var delta = (b - a) / (nodesCount - 1);
+            var x = a + delta;
+            
+            // In fact, here we must use condition x < b, but due to calc errors 
+            // last x = a+delta*(nodesCount-1) can be less than b
+            while (x<b-0.5*delta)
+            {
+                sum += f(x);
+                x += delta;
+            }
+            return sum*delta;
+        }
+
+        public static double Rectangular(Func<double, double> f, double a, double b, int nodesCount, RectType formulaType = RectType.Left)
+        {
+            var nodes = Enumerable.Range(0, nodesCount - 1).Select(j => a + j * (b - a) / (nodesCount - 1)).ToArray();
+            return Rectangular(f, nodes, formulaType);
+        }
+
+        public enum RectType
+        {
+            Left,
+            Right,
+            Center
+        }
+    }
+}
