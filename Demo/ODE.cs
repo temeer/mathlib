ea7ad9d91ca92edfd64d783d06fa9d57ed44e7ff
Rasul <rasuldev@gmail.com>
2016-09-29 10:00:56 +0300

    ODE solver added (Euler method)

--- Demo/ODE.cs
+++ Demo/ODE.cs
@@ -20,13 +20,15 @@ namespace Demo
     {
         GraphBuilder2DForm gb = new GraphBuilder2DForm();
         Plot2D exactSolutionPlot = new Plot2D("Точное решение");
-        Plot2D numSolutionPlot = new Plot2D("Численное решение");
+        Plot2D numSolutionPlotE = new Plot2D("Численное решение методом Эйлера");
+        Plot2D numSolutionPlotEC = new Plot2D("Численное решение методом Эйлера-Коши");
         public ODE()
         {
             InitializeComponent();
             gb.Show();
             gb.GraphBuilder.DrawPlot(exactSolutionPlot);
-            gb.GraphBuilder.DrawPlot(numSolutionPlot);
+            gb.GraphBuilder.DrawPlot(numSolutionPlotE);
+            gb.GraphBuilder.DrawPlot(numSolutionPlotEC);
         }
 
         private void buttonSolve_Click(object sender, EventArgs e)
@@ -40,24 +42,23 @@ namespace Demo
             var y0 = 2d;
             Func<double, double, double> f = (x, y) => 2*x*x + 1/2.0/Sqrt(x);
             var b = 11;
-            var pair = Euler.Solve(f, x0, y0, b, n);
+            var df = Solver.EulerImproved(f, x0, y0, b, n);
+            numSolutionPlotE.DiscreteFunction = df;
+            numSolutionPlotE.Refresh();
 
-            var xarr = pair.Item1;
-            var yarr = pair.Item2;
+            df = Solver.EulerCauchy(f, x0, y0, b, n);
+            numSolutionPlotEC.DiscreteFunction = df;
+            numSolutionPlotEC.Refresh();
 
-            numSolutionPlot.DiscreteFunction = new DiscreteFunction2D(pair.Item1, pair.Item2);
-            numSolutionPlot.Refresh();
-
-            exactSolutionPlot.DiscreteFunction = new DiscreteFunction2D(x => x * x + Sqrt(x), pair.Item1);
+            exactSolutionPlot.DiscreteFunction = new DiscreteFunction2D(x => x * x + Sqrt(x), df.X);
             exactSolutionPlot.Refresh();
-
-
         }
 
         private void cbShowPoints_CheckedChanged(object sender, EventArgs e)
         {
             (gb.GraphBuilder.GetSeriesOfPlot(exactSolutionPlot) as Line).Pointer.Visible = cbShowPoints.Checked;
-            (gb.GraphBuilder.GetSeriesOfPlot(numSolutionPlot) as Line).Pointer.Visible = cbShowPoints.Checked;
+            (gb.GraphBuilder.GetSeriesOfPlot(numSolutionPlotE) as Line).Pointer.Visible = cbShowPoints.Checked;
+            (gb.GraphBuilder.GetSeriesOfPlot(numSolutionPlotEC) as Line).Pointer.Visible = cbShowPoints.Checked;
         }
 
         private void nupPointsCount_ValueChanged(object sender, EventArgs e)
---
+++ mathlib/DiffEq/Solver.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using DiscreteFunctions;
+
+namespace mathlib.DiffEq
+{
+    public static class Solver
+    {
+        /// <summary>
+        /// Solves Cauchy problem y'(x)=f(x,y), y(x0)=y0 on segment [x0,b] using Euler method. 
+        /// </summary>
+        /// <param name="f"></param>
+        /// <param name="x0"></param>
+        /// <param name="y0"></param>
+        /// <param name="b"></param>
+        /// <param name="n">Grid points count</param>
+        /// <returns></returns>
+        public static DiscreteFunction2D Euler(Func<double, double, double> f, double x0, double y0, double b, int n)
+        {
+            var y = new double[n];
+            var x = new double[n];
+            y[0] = y0;
+            x[0] = x0;
+            var h = (b - x0) / (n - 1);
+            var xk = x0;
+            for (int k = 0; k < n-1; k++)
+            {
+                y[k + 1] = y[k] + h * f(x[k], y[k]);
+                x[k + 1] = x[k] + h;
+            }
+            return new DiscreteFunction2D(x, y);
+        }
+
+        /// <summary>
+        /// Solves Cauchy problem y'(x)=f(x,y), y(x0)=y0 on segment [x0,b] using Euler-Cauchy method. 
+        /// </summary>
+        /// <param name="f"></param>
+        /// <param name="x0"></param>
+        /// <param name="y0"></param>
+        /// <param name="b"></param>
+        /// <param name="n">Grid points count</param>
+        /// <returns></returns>
+        public static DiscreteFunction2D EulerCauchy(Func<double, double, double> f, double x0, double y0, double b, int n)
+        {
+            var y = new double[n];
+            var x = new double[n];
+            y[0] = y0;
+            x[0] = x0;
+            var h = (b - x0) / (n - 1);
+            var xk = x0;
+            for (int k = 0; k < n - 1; k++)
+            {
+                y[k + 1] = y[k] + h * f(x[k], y[k]);
+                x[k + 1] = x[k] + h;
+                y[k+1] = y[k] + 0.5*h*(f(x[k], y[k])+f(x[k+1],y[k+1]));
+            }
+            return new DiscreteFunction2D(x, y);
+        }
+
+
+        public static DiscreteFunction2D EulerImproved(Func<double, double, double> f, double x0, double y0, double b,
+            int n)
+        {
+            var y = new double[n];
+            var x = new double[n];
+            y[0] = y0;
+            x[0] = x0;
+            
+            var h = (b - x0) / (n - 1);
+            var xk = x0;
+            for (int k = 0; k < n - 1; k++)
+            {
+                var yhalf = y[k] + 0.5 * h * f(x[k], y[k]);
+                var xhalf = x[k] + 0.5 * h;
+                y[k + 1] = y[k] + h * f(xhalf, yhalf);
+                x[k + 1] = x[k] + h;
+                // y[k + 1] = y[k] + 0.5 * h * (f(x[k], y[k]) + f(x[k + 1], y[k + 1]));
+            }
+            return new DiscreteFunction2D(x, y);
+        }
+
+
+    }
+    
+}
