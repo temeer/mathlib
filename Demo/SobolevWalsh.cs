a36cb10b63e18b57bb539988908918469afa6856
Rasul <rasuldev@gmail.com>
2018-04-03 16:09:49 +0300

    added Walsh - Sobolev functions

--- Demo/SobolevWalsh.cs
+++ Demo/SobolevWalsh.cs
@@ -10,24 +10,34 @@ using System.Windows.Forms;
 using DiscreteFunctions;
 using DiscreteFunctionsPlots;
 using GraphBuilders;
+using mathlib;
 using mathlib.Functions;
+using mathlib.Symbolic;
 
 namespace Demo
 {
     public partial class SobolevWalsh : GraphBuilder2DForm
     {
-        Plot2D walshPlot = new Plot2D("Walsh");
+        private readonly Plot2D _plot = new Plot2D("Walsh - Sobolev");
+        private readonly Plot2D _plot2 = new Plot2D("Walsh - Sobolev - 2^k");
+
         public SobolevWalsh()
         {
             InitializeComponent();
-            GraphBuilder.DrawPlot(walshPlot);
+            GraphBuilder.DrawPlot(_plot);
+            GraphBuilder.DrawPlot(_plot2);
         }
 
         private void nupNum_ValueChanged(object sender, EventArgs e)
         {
-            walshPlot.DiscreteFunction =
-                new DiscreteFunction2D(Walsh.Get((int)nupNum.Value), 0, 1, 1024);
-            walshPlot.Refresh();
+            var n = (int) nupNum.Value;
+            var pow2 = (int) Math.Pow(2, Math.Floor(Math.Log(n-1, 2)));
+            _plot.DiscreteFunction =
+                new DiscreteFunction2D(WalshSobolev.Get(n) , 0, 1, 1024);
+            _plot.Refresh();
+            _plot2.DiscreteFunction =
+                new DiscreteFunction2D(WalshSobolev.Get(pow2+1), 0, 1, 1024);
+            _plot2.Refresh();
         }
     }
 }
---
+++ mathlib/Functions/WalshSobolev.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace mathlib.Functions
+{
+    public class WalshSobolev
+    {
+        public static Func<double, double> Get(int n)
+        {
+            double One(double x) => 1;
+            if (n == 0)
+                return One;
+
+            var nodesCount = (int)Math.Pow(2, (int)Math.Ceiling(Math.Log(n, 2))+6);
+            var nodes = Enumerable.Range(0, nodesCount).Select(j => j*1.0 / nodesCount);
+            double Sobolev(double x)
+            {
+                return Integrals.Rectangular(Walsh.Get(n - 1), nodes.Where(node => node <= x).ToArray(), 
+                    Integrals.RectType.Center);
+            }
+
+            return Sobolev;
+        }
+    }
+}
